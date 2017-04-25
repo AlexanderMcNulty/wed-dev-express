f9187a6e1bdcaf979b92f490969af8a125f0956d
AlexanderMcNulty <AlexanderMcNulty92@gmail.com>
2017-04-24 22:44:02 -0400

    push messy app with templating examples and integration with mongo

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
+var express_session = require('express-session');
 var bodyParser = require('body-parser');
-
+var mongoose = require('mongoose');
+var Vacation = require('./models/vacation.js');
 var index = require('./routes/index');
 var users = require('./routes/users');
+var credentials = require('./credentials.js');
+var VacationInSeasonListener = require('./models/vacationInSeasonListener.js');
 
 var app = express();
 
@@ -19,11 +23,106 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser(credentials.cookieSecret));
+app.use(express_session());
 app.use(express.static(path.join(__dirname, 'public')));
+
+var opts = {
+	server: {
+		socketOptions: { keepAlive: 1}
+	}
+};
+switch(app.get('env')){
+	case 'development':
+		mongoose.connect('mongodb://127.0.0.1:27017/test', opts);
+		break;
+	case 'production':
+		mongoose.connect('mongodb://127.0.0.1:27017/test', opts);
+		break;
+	default:
+		throw new Error('Unknown execution environment: ' + appget('env'));
+}
+Vacation.find(function(err, vacations){
+	if(vacations.length)return;
+	new Vacation({
+		name: 'Hood River Day Trip',
+		slug: 'hood-river-dat-trip',
+		category: 'Day Trip',
+		sku: 'HR199',
+		description: 'Spend a day sailing on the Columbia and ' + 'enjoying craft beers in Hood River!',
+		priceInCents: 9995,
+		tags: ['day trip', 'hood river', 'sailing', 'windsurfing', 'breweries'],
+		inSeason: true,
+		maximumGuest: 16,
+		available:true,
+		packagesSold: 0,
+	}).save();	
+	new Vacation({
+		name: 'do fun stuff',
+		slug: 'hat-trip',
+		category: 'Trip',
+		sku: 'HR555',
+		description: 'Get drunk on craft beers in Hood River!',
+		priceInCents: 9995,
+		tags: ['Get wasted', 'sailing', 'windsurfing', 'breweries'],
+		inSeason: true,
+		maximumGuest: 6,
+		available:true,
+		packagesSold: 0,
+	}).save();
+	new Vacation({
+		name: 'South of No Orgy',
+		slug: 'rip',
+		category: 'rip',
+		sku: 'HR666',
+		description: 'beers in Hood River!',
+		priceInCents: 9995,
+		tags: ['Get wasted', 'orgy', 'sailing', 'windsurfing', 'breweries'],
+		inSeason: false,
+		maximumGuest: 6,
+		available:true,
+		packagesSold: 0,
+		notes: 'oh yeah here is a note',
+	}).save();
+});
+	
+	
 
 app.use('/', index);
 app.use('/users', users);
+app.get('/vacations',function(req,res){
+	Vacation.find({ available:true }, function(err, vacations){
+		var context = {
+			vacations: vacations.map(function(vacation){
+				return { 
+					sku: vacation.sku,	
+					name: vacation.name,
+					description: vacation.description,
+					price: vacation.getDisplayPrice(),
+					inSeason: vacation.inSeason,
+				}
+			})
+		};
+		res.render('vacations',context);
+	});
+});
+app.get('/notify-me-when-in-season', function(req,res){
+        res.render('notify-me-when-in-season', {sku: req.query.sku});
+});
+app.post('/notify-me-when-in-season', function(req,res){
+        VacationInSeasonListener.update(
+                {email: req.body.email},
+                {$push: {skus: req.body.sku} },
+                {upsert: true },
+		function(err){
+			if(err){
+				console.error(err.stack);
+				return res.redirect(303, '/vacations');
+			};
+			return res.redirect(303, '/vacations');
+		}
+        );
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,48 @@
 var express = require('express');
+var formidable = require('formidable');
 var fortunes = require('./lib/fortune.js');
+
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: fortunes.getFortune() });
-  
+	  res.render('index', { title: fortunes.getFortune(), csrf:'CSRF toke goes here'
+	//	console.log('fun stuff');
+	 });
+	 res.cookie('monster','nom nom', {signed:true});  	
+	// res 
+});
+
+router.post('/process',function(req,res){
+	console.log(req.body);
+	var email = req.body.email;
+	var name = req.body.name;
+	req.session.name = name;
+	console.log('\n\n'+ req.signedCookies.monster);
+	res.redirect(303,'/results');
+});
+
+router.get('/contest/vacation-photo', function(req,res){
+	var now = new Date();
+	res.render('contest/vacation-photo',{
+		year: now.getFullYear(),month: now.getMont()
+	});
+});
+
+router.post('/contest/vacation-photo', function(req,res){
+	var form = new formidable.IncomingForm();
+	form.parse(req,function(err,fields,files){
+		if(err) return res.redirect(303, '/error');
+		console.log('received fields:');
+		console.log(fields);
+		console.log('received files:');
+		console.log(files);
+		res.redirect(303, '/results');
+	});
+});
+router.get('/results', function(req, res, next){
+	res.render('results', {content1: req.session.name});
+	console.log(req.body);
 });
 
 module.exports = router;
